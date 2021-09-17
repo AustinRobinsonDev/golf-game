5d02b25a3306dc82c647283cb66d48ecabd509a0
AustinRobinsonDev <austin.worktrux@gmail.com>
2021-09-17 10:50:09 -0500

    replace nav with bootstrap nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import Game from './components/Game';
-import { Container, Row } from 'react-bootstrap';
-import Navbar from './components/Navbar'
+import { Container, Row, Navbar } from 'react-bootstrap';
+//import Navbar from './components/Navbar'
 
 
 function App() {
   const [id, setId] = useState();
   return (
     <div className='app'>
-    <Navbar />
+    <Navbar expand="lg" variant="light" bg="light">
+  <Container>
+    <Navbar.Brand href="#">Golf, Uht!</Navbar.Brand>
+  </Container>
+</Navbar>
+    {/* <Navbar /> */}
     {id}
     {/* <Login onIdSubmit={setId}/> */}
     <Game />
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -6,21 +6,17 @@ const Game = () => {
     let holeCount = 1;
     return (
         <Container className="w-100 d-flex flex-column align-items-center p-0" style={{ height: '100vh'}}>
-            <Container className='d-flex justify-content-center bg-secondary w-100 '>
-            </Container>
-            <h1>Hole {holeCount}</h1>
-            <Container className='d-flex flex-column align-items-start mt-5 py-3 mOpacity stats' style={{maxWidth: '350px'}}>
-                <Container className='w-50'>
-                    <h4>Length: 475</h4>
-                    <h4>Par: 5</h4>
+
+ 
+                <Container className='w-100 d-flex m-0 flex-column align-items-center stats pt-4'>
+                    <h1 className='mb-3 mt-3'>Hole {holeCount}</h1>
+                    <h4 className='mb-3' style={{color: 'white'}}>Length: 475</h4>
+                    <h4 className='text-warning'>Par: 5</h4>
                 </Container>
                 <div className='mx-auto mt-5'>
                     <p className='text-center'>Each player must tee off of a beer can.</p>
                 </div>
-            </Container>
-            <div>
 
-            </div>
 
         </Container>
     )
